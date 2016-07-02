e8c19cd0df647881ddb0404564ee5dcc8413292e
Chenxi <chenxicheer@gmail.com>
2016-07-02 16:09:00 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'static_pages#home'
+    root 'static_pages#home'
+    resources :users
 
 
   # Example of regular route:
